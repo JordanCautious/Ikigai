f99b07b413d30a69edd573b607dfc80bed984337
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2022-08-19 14:23:55 -0400

    Initial Commit

---
+++ Ikigai/IkigaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IkigaiApp.swift
+//  Ikigai
+//
+//  Created by Jordan Haynes on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct IkigaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
